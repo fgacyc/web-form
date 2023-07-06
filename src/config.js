147072911cbdb4f4c1ec54d4a0c02d155ee97a35
Innis <1575270674@qq.com>
2023-07-06 19:42:48 +0800

    update: hostURL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-export  const hostURL = "https://api.enjoycoding.me"
+// export  const hostURL = "https://api.enjoycoding.me"
  //export const hostURL = "http://localhost:6023"
+
+export  const hostURL = "https://api.enjoycoding.me/dev";
