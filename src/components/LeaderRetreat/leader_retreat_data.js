5fadbebb63dfcdba1e85aa71a463304329d3330f
Innis <1575270674@qq.com>
2023-08-02 18:59:36 +0800

    update: change retreat card data

--- src/components/LeaderRetreat/leader_retreat_data.js
+++ src/components/LeaderRetreat/leader_retreat_data.js
@@ -2,15 +2,9 @@ export const faq_data = [
     {
         question: 'What is the fee?',
         answer: [
-            'Leader’s Retreat is RM200/pax for adults, and RM75/pax for kids (6-12 years old)',
-            'Kids that are 5 years old and below will be free of charged.',
+            'Leader’s Retreat is RM200/pax for adults. RM80 for kids 7-12 years old.',
+            'Kids that are 6 years old and below will be free of charged.',
             'Fee includes accommodation, 1x breakfast, 2 lunches and 2 dinners. Transport not included.'
-        ]
-    },
-    {
-        question: 'Will there be child care?',
-        answer: [
-            'Yes, child care will be provided during the Leader’s Retreat. However, toddlers that are 2 years old and below are expected to be taken care by the parents.'
         ]
     },
     {
@@ -29,7 +23,7 @@ export const faq_data = [
     {
         question: 'Will there be live streaming?',
         answer: [
-            'Live streaming won’t be available for the entirety of the Leader’s Retreat.'
+            'Live streaming won’t be available for the entirety of the Leader’s Retreat. '
         ]
     }
 ]
