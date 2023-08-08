e6a4cd35f6623cf15767c215e86551c43de9c930
Yannis Leng <lengzhiyan1015@gmail.com>
2023-08-08 16:47:31 +0800

    feat: wonderkids ministry registration

--- src/components/Submission/Submission.jsx
+++ src/components/Submission/Submission.jsx
@@ -12,18 +12,26 @@ import { findMinistryNeeds } from "../../data/ministry_needs.js"; // Import css
 
 export default function Submission() {
     const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+    const [registerForChild, setRegisterForChild] = useState(false);
 
     const navigate = useNavigate();
 
     const [name, setName] = useState("");
+    const [guardian, setGuardian] = useState("");
     const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     const [pastoralTeam, setPastoralTeam] = useState("");
+    const [leader, setLeader] = useState("");
+    const [instrument, setInstrument] = useState("");
+
     const [nameError, setNameError] = useState("");
+    const [guardianError, setGuardianError] = useState("");
     const [phoneError, setPhoneError] = useState("");
     const [emailError, setEmailError] = useState("");
     const [pastoralTeamError, setPastoralTeamError] = useState("");
+    const [leaderError, setLeaderError] = useState("");
     const [selectedMinistry, setSelectedMinistry] = useState(null);
+    const [instrumentError, setInstrumentError] = useState("");
 
     useEffect(() => {
         const selectedMinistry = JSON.parse(localStorage.getItem('cyc-department-selected'));
@@ -44,12 +52,19 @@ export default function Submission() {
     }, []);
 
 
-    const validateName = () => {
-        if (name.trim() === "") {
-            setNameError("Name is required");
+    const validateField = (field, setFieldError, errorTxt) => {
+        const regex = /^[A-Za-z\s]+$/;
+
+        if (field.trim() === "") {
+            setFieldError(errorTxt);
             return false;
         }
-        setNameError("");
+
+        if (!regex.test(field)) {
+            setFieldError('Field contains alphabet characters only');
+            return false;
+        }
+        setFieldError("");
         return true;
     };
 
@@ -143,17 +158,17 @@ export default function Submission() {
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        let ifStop = true;
+        // let ifStop = true;
 
-        if (ifStop) {
-            alert("Submission is finished. Please wait for the announcement.");
-            return;
-        }
+        // if (ifStop) {
+        //     alert("Submission is finished. Please wait for the announcement.");
+        //     return;
+        // }
 
-        if (isButtonDisabled) {
-            alert("Submission is not open yet. Please wait for the announcement.");
-            return;
-        }
+        // if (isButtonDisabled) {
+        //     alert("Submission is not open yet. Please wait for the announcement.");
+        //     return;
+        // }
 
         // const ifSubmitted = "false"
         // if (ifSubmitted === "true") {
@@ -161,14 +176,29 @@ export default function Submission() {
         //     return;
         // }
 
-        const isNameValid = validateName();
+        const isNameValid = validateField(name, setNameError, "Name is required");
         const isPhoneValid = validatePhone();
         const isEmailValid = validateEmail();
-        const isPastoralTeamValid = validatePastoralTeam();
 
-        // console.log(isPhoneValid)
+        let isPastoralTeamValid = true;
+        let isGuardianValid = true;
+        let isLeaderValid = true;
+        let isInstrumentValid = true;
 
-        if (isNameValid && isPhoneValid && isEmailValid && isPastoralTeamValid) {
+        if (registerForChild) {
+            isGuardianValid = validateField(guardian, setGuardianError, "Guardian is required");
+            isLeaderValid = validateField(leader, setLeaderError, "Leader is required");
+            isInstrumentValid = validateField(instrument, setInstrumentError, "Instrument is required");
+            setPastoralTeam("wonderkids");
+            if (selectedMinistry === 'vocal') {
+                setInstrument("none");
+            }
+        } else {
+            isPastoralTeamValid = validatePastoralTeam();
+        }
+
+        if (isNameValid && isPhoneValid && isEmailValid && isPastoralTeamValid
+            && isGuardianValid && isLeaderValid && isInstrumentValid) {
             confirmAlert(options);
         }
     };
@@ -176,26 +206,43 @@ export default function Submission() {
     function submitToServer() {
         let pastoralTeamList = findPastoralTeam(pastoralTeam);
         let ministryList = findMinistry(selectedMinistry);
-        let info = {
-            "name": name,
-            "phone": phone,
-            "email": email,
-            "pastoral_team": pastoralTeamList,
-            "ministry": ministryList
-        }
-        // console.log(info);
 
-        postRecruiter(info).then((result) => {
-            if (result === true) {
-                navigate("/complete");
-                localStorage.setItem("cyc-submission", "true")
-            } else if (result === "failed") {
-                alert("You have already submitted your application. Please wait for the response.")
-                navigate("/");
-            } else {
-                alert("Something went wrong, Please try again.");
+        let info;
+
+        if (registerForChild) {
+            info = {
+                "name": name,
+                "guardian": guardian,
+                "phone": phone,
+                "email": email,
+                "leader": leader,
+                "instrument": instrument,
+                "pastoral_team": pastoralTeamList,
+                "ministry": ministryList
             }
-        });
+        } else {
+            info = {
+                "name": name,
+                "phone": phone,
+                "email": email,
+                "pastoral_team": pastoralTeamList,
+                "ministry": ministryList
+            }
+        }
+
+        console.log(info);
+
+        // postRecruiter(info).then((result) => {
+        //     if (result === true) {
+        //         navigate("/complete");
+        //         localStorage.setItem("cyc-submission", "true")
+        //     } else if (result === "failed") {
+        //         alert("You have already submitted your application. Please wait for the response.")
+        //         navigate("/");
+        //     } else {
+        //         alert("Something went wrong, Please try again.");
+        //     }
+        // });
     }
 
     const options = {
@@ -232,12 +279,35 @@ export default function Submission() {
                         Submission
                     </h3>
                 </div>
+                {
+                    (selectedMinistry === 'vocal' || selectedMinistry === 'musician') && (
+                        <div className='flex align-center'>
+                            <input
+                                type="checkbox"
+                                className='child-checkbox'
+                                checked={registerForChild}
+                                onChange={() => setRegisterForChild(!registerForChild)}
+                            />
+                            <div className='child-register'>Registering for child?</div>
+                        </div>
+                    )
+                }
                 <form action="#" className="flex flex-col submission-form">
                     <label htmlFor="name" className="input-text">Full Name</label>
                     <input type="text" name="name" id="name" value={name} onChange={(e) => setName(e.target.value)} />
                     {nameError && <div className="input-error">{nameError}</div>}
 
-                    <label htmlFor="phone" className="input-text">Phone</label>
+                    {
+                        registerForChild && (
+                            <>
+                                <label htmlFor="guardian" className="input-text">Parent/ Guardian Name</label>
+                                <input type="text" name="guardian" id="guardian" value={guardian} onChange={(e) => setGuardian(e.target.value)} />
+                                {guardianError && <div className="input-error">{guardianError}</div>}
+                            </>
+                        )
+                    }
+
+                    <label htmlFor="phone" className="input-text">Contact Number</label>
                     <input type="text" name="phone" id="phone" value={phone} onChange={(e) => setPhone(e.target.value)} />
                     {phoneError && <div className="input-error">{phoneError}</div>}
 
@@ -245,43 +315,74 @@ export default function Submission() {
                     <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                     {emailError && <div className="input-error">{emailError}</div>}
 
-                    <label htmlFor="pastoral_team" className="input-text">Pastoral Team</label>
-                    <select
-                        className="submission-select"
-                        name="pastoral_team"
-                        id="pastoral_team"
-                        value={pastoralTeam}
-                        onChange={pastoralTeamHandler}
-                    >
-                        <option value="" disabled hidden>Select a pastoral team</option>
-                        <optgroup label="Wonderkids">
-                            <option value="wonderkids">Wonderkids</option>
-                        </optgroup>
-                        <optgroup label="Young Warrior">
-                            <option value="heart">Heart</option>
-                            <option value="move">Move</option>
-                            <option value="force">Force</option>
-                            <option value="voice">Voice</option>
-                            <option value="mind">Mind</option>
-                        </optgroup>
-                        <optgroup label="General">
-                            <option value="yp_zone">YP Zone</option>
-                            <option value="pro_family">Pro Family</option>
-                            <option value="young_dreamer">Young Dreamer</option>
-                            <option value="joshua_zone">Joshua Zone</option>
-                        </optgroup>
-                        <optgroup label="Others">
-                            <option value="serdang">Serdang</option>
-                            <option value="kepong">Kepong</option>
-                            <option value="usj">USJ</option>
-                            <option value="setapak">Setapak</option>
-                            <option value="sg_long">Sg Long</option>
-                            <option value="seremban">Seremban</option>
-                            <option value="the_blessing">The Blessing</option>
-                            <option value="to_be_confirmed">To Be Confirmed</option>
-                        </optgroup>
-                    </select>
-                    {pastoralTeamError && <div className="input-error">{pastoralTeamError}</div>}
+                    {
+                        registerForChild ?
+                            <>
+                                <label htmlFor="leader" className="input-text">Leader</label>
+                                <select
+                                    className="submission-select"
+                                    name="leader"
+                                    id="leader"
+                                    value={leader}
+                                    onChange={(e) => setLeader(e.target.value)}
+                                >
+                                    <option value="" disabled hidden>Select a leader</option>
+                                    <option value="leaderOne">Leader 1</option>
+                                    <option value="leaderTwo">Leader 2</option>
+                                </select>
+                                {leaderError && <div className="input-error">{leaderError}</div>}
+                            </>
+                            :
+                            <>
+                                <label htmlFor="pastoral_team" className="input-text">Pastoral Team</label>
+                                <select
+                                    className="submission-select"
+                                    name="pastoral_team"
+                                    id="pastoral_team"
+                                    value={pastoralTeam}
+                                    onChange={pastoralTeamHandler}
+                                >
+                                    <option value="" disabled hidden>Select a pastoral team</option>
+                                    <optgroup label="Wonderkids">
+                                        <option value="wonderkids">Wonderkids</option>
+                                    </optgroup>
+                                    <optgroup label="Young Warrior">
+                                        <option value="heart">Heart</option>
+                                        <option value="move">Move</option>
+                                        <option value="force">Force</option>
+                                        <option value="voice">Voice</option>
+                                        <option value="mind">Mind</option>
+                                    </optgroup>
+                                    <optgroup label="General">
+                                        <option value="yp_zone">YP Zone</option>
+                                        <option value="pro_family">Pro Family</option>
+                                        <option value="young_dreamer">Young Dreamer</option>
+                                        <option value="joshua_zone">Joshua Zone</option>
+                                    </optgroup>
+                                    <optgroup label="Others">
+                                        <option value="serdang">Serdang</option>
+                                        <option value="kepong">Kepong</option>
+                                        <option value="usj">USJ</option>
+                                        <option value="setapak">Setapak</option>
+                                        <option value="sg_long">Sg Long</option>
+                                        <option value="seremban">Seremban</option>
+                                        <option value="the_blessing">The Blessing</option>
+                                        <option value="to_be_confirmed">To Be Confirmed</option>
+                                    </optgroup>
+                                </select>
+                                {pastoralTeamError && <div className="input-error">{pastoralTeamError}</div>}
+                            </>
+                    }
+
+                    {
+                        registerForChild && selectedMinistry === 'musician' && (
+                            <>
+                                <label htmlFor="instrument" className="input-text">Instrument(s)</label>
+                                <input type="text" name="instrument" id="instrument" value={instrument} onChange={(e) => setInstrument(e.target.value)} />
+                                {instrumentError && <div className="input-error">{instrumentError}</div>}
+                            </>
+                        )
+                    }
                 </form>
             </div>
             <div className='flex flex-col align-center'>
