76b76776b9be50918eb62381e4fc152c11cb88d8
ETCasual <casualorient@gmail.com>
2023-09-09 23:05:57 +0800

    remove auth0 due to authorization error

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,8 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LeaderRetreat from "./components/LeaderRetreat/LeaderRetreat";
 import LeaderRetreatRegister from "./components/LeaderRetreat/Register";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
 
 function App() {
-  const { getAccessTokenSilently, user, isLoading } = useAuth0();
-
-  useEffect(() => {
-    if (isLoading) return;
-
-    user &&
-      getAccessTokenSilently().then((tkn) =>
-        sessionStorage.setItem("fgacyc-auth-token", tkn)
-      );
-  }, [getAccessTokenSilently, isLoading, user]);
-
   return (
     <Routes>
       <Route path="/" element={<LeaderRetreat />} />
--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={String(import.meta.env.VITE_AUTH0_DOMAIN)}
-      clientId={String(import.meta.env.VITE_AUTH0_CLIENT_ID)}
-      authorizationParams={{
-        redirect_uri: `${window.location.origin}/register`,
-      }}
-      cacheLocation="localstorage"
-    >
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Auth0Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
