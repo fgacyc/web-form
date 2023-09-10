83ea1edd668f082f65b12dff8bdccfc8ac9e9e20
ETCasual <casualorient@gmail.com>
2023-09-10 19:34:30 +0800

    update head file

--- src/config.js
+++ src/config.js
@@ -2,13 +2,12 @@
 //export const hostURL = "http://localhost:6023"
 
 // cloud prod
-export const hostURL = "https://api.enjoycoding.me";
-
+// export const hostURL = "https://api.enjoycoding.me";
 
 //cloud dev
 // export  const hostURL = "https://api.enjoycoding.me/dev";
 
 //dev
- //export const hostURL = "http://localhost:6023/dev";
+//export const hostURL = "http://localhost:6023/dev";
 
 //export const hostURL = "https://auth.fgacyc.com";
