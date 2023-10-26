bcb60869e0509cd184c1c07ebd44c6c03ae581d5
ETCasual <casualorient@gmail.com>
2023-10-26 18:08:34 +0800

    update

--- src/components/LeaderRetreat/LeaderRetreat.jsx
+++ src/components/LeaderRetreat/LeaderRetreat.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import { useState, useEffect } from "react";
+import { roomArrangement } from "../../data/rooms";
 import { faq_data } from "./leader_retreat_data";
 import ReactFullpage from "@fullpage/react-fullpage";
 import "./leaderRetreat.css";
-import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
+import { SiWaze, SiGooglemaps } from "react-icons/si";
 
 const LeaderRetreat1 = ({ onClick }) => {
   return (
@@ -25,80 +26,131 @@ const LeaderRetreat1 = ({ onClick }) => {
         <div className="retreat-date">&quot;CHOSEN 300&quot;</div>
       </div>
       <div className="mb-75">
-        <div className="btn-retreat-title-text">28 - 29 OCT 2023</div>
-        <button className="btn-retreat relative" onClick={onClick}>
-          SCROLL
+        <button className="btn-retreat  relative" onClick={onClick}>
+          28 - 29 OCT 2023
         </button>
       </div>
     </section>
   );
 };
 
-const LeaderRetreat2 = () => {
+const LeaderRetreat2 = ({ onClick }) => {
   return (
     <section
       className="section flex flex-col items-center justify-between retreat-bg-2"
       // onTouchStart={handleTouchStart}
       // onTouchEnd={handleTouchEnd}
     >
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center mt-12rem">
+        <div className="welcome-msg">Welcome to</div>
         <div className="retreat-2-text-1 bg-blend-difference">
           BAYOU LAGOON PARK RESORT
         </div>
         <div className="retreat-2-text-2">MELAKA</div>
       </div>
+
+      <div
+        className="mb-75"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <button
+          className="btn-retreat maps relative"
+          onClick={() =>
+            window.open(
+              "https://www.google.com/maps?q=bayou+lagoon&rlz=1C1CHBD_enMY1079MY1079&gs_lcrp=EgZjaHJvbWUqFggBEC4YrwEYxwEYsQMYgAQYmAUYmgUyBggAEEUYOTIWCAEQLhivARjHARixAxiABBiYBRiaBTITCAIQLhivARjHARiABBiYBRiZBTINCAMQLhivARjHARiABDIHCAQQABiABDIHCAUQABiABDIHCAYQABiABDINCAcQLhivARjHARiABDINCAgQLhivARjHARiABDIHCAkQABiPAtIBCjEwMzY1NmowajeoAgCwAgA&um=1&ie=UTF-8&sa=X&ved=2ahUKEwiBm8yFt5OCAxWIM0QIHfpMDWcQ_AUoAnoECAMQBA"
+            )
+          }
+        >
+          <p
+            style={{
+              margin: "0",
+              verticalAlign: "middle",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            Google Maps
+            <SiGooglemaps style={{ marginLeft: "10px" }} />
+          </p>
+        </button>
+        <button
+          className="btn-retreat waze relative"
+          onClick={() => window.open("https://waze.com/ul/hw22ub02su")}
+        >
+          <p
+            style={{
+              margin: "0",
+              verticalAlign: "middle",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+            }}
+          >
+            WAZE
+            <SiWaze style={{ marginLeft: "10px" }} />
+          </p>
+        </button>
+        <button className="btn-retreat relative" onClick={onClick}>
+          Scroll
+        </button>
+      </div>
     </section>
   );
 };
 
-const LeaderRetreat3 = ({
-  openCollapse,
-  handleToggle,
-  // handleTouchStart,
-  // handleTouchEnd,
-  // handleSubmit,
-  // isAuthenticated,
-}) => {
+const LeaderRetreat3 = () => {
   return (
     <section
-      className="section relative flex flex-col items-center justify-between retreat-bg-3"
+      className="section section-3 relative flex flex-col items-center justify-between retreat-bg-3"
       // onTouchStart={handleTouchStart}
       // onTouchEnd={handleTouchEnd}
     >
-      <div className="retreat-3-text-1">FAQ</div>
+      <div className="section-3-title">
+        <div className="retreat-3-text-1">RETREAT</div>
+        <div className="retreat-3-text-1">SCHEDULE</div>
+      </div>
 
-      <div
-        style={{
-          width: "100%",
-          padding: "0 20px",
-          display: "flex",
-          flexDirection: "column",
-          gap: "10px",
-          maxWidth: "390px",
-        }}
-      >
-        {faq_data.map((data, index) => {
-          return (
-            <Collapse
-              key={index}
-              title={data.question}
-              content={data.answer}
-              isOpen={openCollapse === index}
-              onToggle={() => handleToggle(index)}
-            />
-          );
-        })}
+      <div className="schedule-box">
+        <div className="schedule-main-title">28 OCT (SAT)</div>
+        <div className="schedule-item">11.00AM — Doors Open</div>
+        <div className="schedule-item">11.15AM — Pre-Show</div>
+        <div className="schedule-item">11.30AM — Session 1</div>
+        <div className="schedule-item">11.30PM — Lunch + Check In</div>
+        <div className="schedule-item">3.00PM — Team Building</div>
+        <div className="schedule-item">5.00PM — Break</div>
+        <div className="schedule-item">6.30PM — Dinner</div>
+        <div className="schedule-item">7.30PM — Doors Open</div>
+        <div className="schedule-item">8.00PM — Session 2</div>
+        <div className="h-17" />
+        <div className="schedule-main-title">29 OCT (SUN)</div>
+        <div className="schedule-item">7.00AM — Breakfast</div>
+        <div className="schedule-item">9.00AM — Doors Open</div>
+        <div className="schedule-item">9.30AM — Session 3 (Part 1)</div>
+        <div className="schedule-item">11.00AM — Session 3 (Part 2)</div>
+        <div className="schedule-item">12.00PM — Lunch + Check Out</div>
+        <div className="schedule-item">1.00PM — Session 4</div>
+        <div className="schedule-item">3.00PM — End</div>
+        <div className="h-20" />
+        <div className="schedule-main-title kids-title">KIDS PROGRAM</div>
+        <div className="schedule-item">
+          <span className="schedule-head">28 OCT — </span>11.00AM, 3.00PM,
+          7.30PM
+        </div>
+        <div className="schedule-item">
+          <span className="schedule-head">29 OCT — </span>9.00AM
+        </div>
       </div>
       <div className="align-center my-30 flex flex-col">
         <img src="/images/retreat_title.png" alt="Leader's Retreat Title" />
 
-        <button
+        {/* <button
           style={{ marginTop: "25px" }}
           className="btn-retreat btn-disabled"
           disabled
         >
           Registrations closed.
-        </button>
+        </button> */}
 
         {/* <button
           className={`btn-retreat ${isAuthenticated ? "activated" : ""}`}
@@ -107,6 +159,211 @@ const LeaderRetreat3 = ({
         >
           {isAuthenticated ? "REGISTER" : "SIGN IN"}
         </button> */}
+      </div>
+    </section>
+  );
+};
+
+const LeaderRetreat4 = ({ openCollapse, handleToggle }) => {
+  return (
+    <section
+      className="section section-3 relative flex flex-col items-center retreat-bg-1"
+      // onTouchStart={handleTouchStart}
+      // onTouchEnd={handleTouchEnd}
+    >
+      <div className="wrapper">
+        {/* <div className='retreat-overlay'></div> */}
+        <div className="section-3-title">
+          <div className="retreat-4-text-1">FAQ</div>
+        </div>
+
+        <div
+          style={{
+            width: "100%",
+            // padding: "0 20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            maxWidth: "390px",
+          }}
+        >
+          {faq_data.map((data, index) => {
+            return (
+              <Collapse
+                key={index}
+                title={data.question}
+                content={data.answer}
+                isOpen={openCollapse === index}
+                onToggle={() => handleToggle(index)}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+const LeaderRetreat5 = () => {
+  const [searchString, setSearchString] = useState("");
+  return (
+    <section
+      className="section section-3 relative flex flex-col items-center retreat-bg-1"
+      // onTouchStart={handleTouchStart}
+      // onTouchEnd={handleTouchEnd}
+    >
+      <div className="wrapper">
+        {/* <div className='retreat-overlay'></div> */}
+        <div className="section-3-title">
+          <div className="retreat-4-text-1">ROOM ARRANGEMENT</div>
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "15px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              borderRadius: "10px",
+              overflow: "hidden",
+              background: "#b3b3b3",
+            }}
+          >
+            <label
+              style={{
+                color: "white",
+                padding: "0 0.5rem",
+                textTransform: "uppercase",
+              }}
+            >
+              Name
+            </label>
+            <input
+              style={{
+                outline: "none",
+                padding: "0.75rem 1rem",
+                width: "100%",
+                fontFamily: "SF Pro Display",
+                fontSize: "16px",
+                letterSpacing: "0.025em",
+                border: "none",
+              }}
+              onChange={(e) => setSearchString(e.currentTarget.value)}
+            />
+          </div>
+          <div
+            onTouchStart={(e) => e.stopPropagation()}
+            onTouchEnd={(e) => e.stopPropagation()}
+            onTouchMove={(e) => e.stopPropagation()}
+            onScroll={(e) => e.stopPropagation()}
+            onDrag={(e) => e.stopPropagation()}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "10px",
+              maxHeight: "80vh",
+              overflow: "scroll",
+              width: "100%",
+            }}
+          >
+            {Object.entries(roomArrangement)
+              .filter(([, roomOccupants]) => {
+                const matchingOccupants = roomOccupants.filter((occupant) =>
+                  occupant.toLowerCase().includes(searchString.toLowerCase())
+                );
+                return matchingOccupants.length > 0;
+              })
+              .map(([roomName, roomOccupants]) => {
+                const name = roomName.split(" - ")[0];
+
+                return (
+                  <div
+                    key={roomName}
+                    style={{
+                      borderRadius: "10px",
+                      background: "#000",
+                      display: "flex",
+                      flexDirection: "column",
+                      width: "100%",
+                      padding: "1px",
+                    }}
+                  >
+                    <label
+                      style={{
+                        color: "#fff",
+                        fontFamily: "SF Pro Display",
+                        fontSize: "18px",
+                        paddingLeft: "10px",
+                        paddingTop: "10px",
+                        paddingBottom: "10px",
+                      }}
+                    >
+                      {name === "BR2"
+                        ? "2 BEDROOMS"
+                        : name === "BR3"
+                        ? "3 BEDROOMS"
+                        : name}
+                    </label>
+                    <div
+                      style={{
+                        borderRadius: "10px",
+                        background: "rgba(255,255,255,0.5)",
+                        display: "flex",
+                        flexDirection: "column",
+                        gap: "4px",
+                        padding: "6px",
+                      }}
+                    >
+                      {roomOccupants.map((d) => (
+                        <div
+                          style={{
+                            fontFamily: "SF Pro Display",
+                            fontSize: "18px",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            whiteSpace: "nowrap",
+                          }}
+                          key={d}
+                        >
+                          {d}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        {/* <div
+          style={{
+            width: "100%",
+            // padding: "0 20px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "10px",
+            maxWidth: "390px",
+          }}
+        >
+          {faq_data.map((data, index) => {
+            return (
+              <Collapse
+                key={index}
+                title={data.question}
+                content={data.answer}
+                isOpen={openCollapse === index}
+                onToggle={() => handleToggle(index)}
+              />
+            );
+          })}
+        </div> */}
       </div>
     </section>
   );
@@ -140,7 +397,6 @@ const Collapse = ({ title, content, isOpen, onToggle }) => {
 
 export default function LeaderRetreat() {
   const [openCollapse, setOpenCollapse] = useState();
-  const navigate = useNavigate();
   useEffect(() => {
     document.querySelector(".fp-watermark")?.classList.add("d-none");
     document
@@ -151,7 +407,7 @@ export default function LeaderRetreat() {
   const handleToggle = (index) => {
     setOpenCollapse((prevIndex) => (prevIndex === index ? undefined : index));
   };
-  const { isAuthenticated, loginWithRedirect, isLoading, logout } = useAuth0();
+  const { isAuthenticated, isLoading, logout } = useAuth0();
 
   useEffect(() => {
     if (!isAuthenticated || isLoading) return;
@@ -167,21 +423,13 @@ export default function LeaderRetreat() {
             api={fullpageApi}
             onClick={() => fullpageApi.moveSectionDown()}
           />
-          <LeaderRetreat2
-          // handleTouchStart={handleTouchStart}
-          // handleTouchEnd={handleTouchEnd(1)}
-          />
-          <LeaderRetreat3
+          <LeaderRetreat2 onClick={() => fullpageApi.moveSectionDown()} />
+          <LeaderRetreat3 />
+          <LeaderRetreat4
             openCollapse={openCollapse}
             handleToggle={handleToggle}
-            isAuthenticated={isAuthenticated}
-            // handleTouchStart={handleTouchStart}
-            // handleTouchEnd={handleTouchEnd(2)}
-            handleSubmit={() => {
-              if (isAuthenticated) navigate("/register");
-              if (!isAuthenticated) loginWithRedirect();
-            }}
           />
+          <LeaderRetreat5 />
         </ReactFullpage.Wrapper>
       )}
     />
--- src/components/LeaderRetreat/leader_retreat_data.js
+++ src/components/LeaderRetreat/leader_retreat_data.js
@@ -1,34 +1,53 @@
 export const faq_data = [
   {
-    question: "What is the fee?",
+    question: "Are there any parking available?",
     answer: [
-      "Adults: RM200/pax",
-      "Kids (4-12YO): RM125/pax",
-      "Kids that are 3 years old and below will be FOC (without bed).",
-      "Fee includes accommodation, 1x breakfast, 2 lunches and 1 dinner.",
+      "The resort provides parking facilities for guests and it is free of charge.",
     ],
   },
   {
-    question: "What about transport?",
-    answer: ["Transport will be arranged individually."],
-  },
-  {
-    question: "How is the schedule like?",
+    question: "About Room Check-In / Checkout",
     answer: [
-      "Sessions will begin Saturday afternoon and conclude by Sunday evening.",
-      "A detailed program with session timings will be made available closer to the event date!",
+      "Room check-in and out will be done at the counter near the entrance of the Main Hall.",
+      "Check-In: Saturday (28 Oct), 1.30pm - 2.00pm",
+      "Check-Out: Sunday (29 Oct), 12pm - 12.30pm",
+      "You can leave your luggage in your car or at the main hall upon arrival before joining Session 1 at 11am.",
+      "If the check-in counter is closed, kindly contact Qian Yi (016-9644290) or Phoebe (018-3111107)",
     ],
   },
   {
-    question: "Will there be live streaming?",
+    question: "Any dresscode for the sessions?",
     answer: [
-      "Live streaming won’t be available for the entirety of the Leader’s Retreat. ",
+      "Dress smart and neatly; no slippers and short pants during the sessions.",
+      "We will be taking group photos after the session; feel free to set your team's dresscode.",
+      "Saturday (2.30pm - 3pm): Serdang, Kepong, USJ, Setapak, Sg Long, Seremban",
+      "Saturday (5.00pm - 5.30pm): Kuchai Wonderkids, Young Warrior, General Service & The Blessing.",
+      "Sunday (12pm - 12.30pm): All CYC Ministry Crew, CYC Full Timer.",
     ],
   },
   {
-    question: "Will there be WonderKids?",
+    question: "About Kids",
     answer: [
-      "Yes, Wonderkids programs will be provided for kids aged 3-10 years old. Kids aged 2 and below will be taken care by the parents.",
+      "For kids aged 4 to 10, we have Kids Program prepared in the hall next to our Main Hall. Please refer to the schedule for the time slot.",
+      "Gentle reminder: Pick up your kids within 15 minutes after each session ends. Bring a water bottle and jacket for your kids.",
+      "As for kids below 4 and between 11 to 12 years old, they can sit with their parents during the session. However, do ensure the children remain seated and does not create disturbance to the speaker and other attendees.",
+    ],
+  },
+  {
+    question: "About Interpretation Services",
+    answer: [
+      "Should you need interpretation service,",
+      "Kindly bring your own earphone for the device.",
+      "For device collection or any relavant questions, please contact Andre Him (012-2005297)",
+    ],
+  },
+  {
+    question: "About First Aids",
+    answer: [
+      "Should you require first aid or any medical assistance,",
+      "Kindly contact:",
+      "Mei Ling (016-626 2598)",
+      "Wui Lun (012-799 7189)",
     ],
   },
 ];
